450adb10467e51c49c873f618d1169c067f56424
Daniele Guido <gui.daniele@gmail.com>
2018-10-22 22:50:08 +0200

    add redirection, if any

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
+const debug = require('debug')('express-contact-form/app');
+const config = require('config');
 const express = require('express');
 
 const app = express();
 
 const bodyparser = require('body-parser');
 const request = require('request');
-const config = require('config');
+
+const { check, validationResult } = require('express-validator/check');
 
 const recaptcha = config.get('recaptcha');
+const redirect = config.get('redirect');
 const mailer = require('./mailer');
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.post('/', (req) => {
+
+app.post('/', [
+  // username must be an email
+  check('email').isEmail(),
+  // password must be at least 5 chars long
+  check('contents').isLength({
+    min: 5,
+    max: 500,
+  })
+], (req, res) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    if(redirect.BadRequest) {
+      return res.redirect(redirect.BadRequest);
+    } return res.status(404).json({
+      errors: errors.array()
+    });
+  }
+  debug('POST remote:', req.connection.remoteAddress);
+  debug('POST body:', req.body);
+
   request({
     url: recaptcha.url,
     qs: {
@@ -20,15 +45,23 @@ app.post('/', (req) => {
       response: req.body['g-recaptcha-response'],
       remoteip: req.connection.remoteAddress,
     },
-  }, (err, res, body) => {
+  }, (err, recaptchaResponse, body) => {
     if (err) {
-      return res.send({ message: 'geeric error' });
+      console.log(err);
+      return res.status(404).json({
+        message: 'generic error'
+      });
     }
     const b = JSON.parse(body);
 
     if (b.success !== undefined && !b.success) {
-      mailer();
-      return res.send({ message: 'Captcha validation failed' });
+      console.log(b)
+      return res.status(404).json({
+        message: 'Captcha validation failed'
+      });
+    }
+    if(redirect.Success) {
+      return res.redirect(redirect.Success);
     }
     res.header('Content-Type', 'application/json');
     return res.send(b);
