c22f5c5c8818c3d8ce19e54198baf75bea56bfb8
Daniele Guido <gui.daniele@gmail.com>
2018-10-23 23:08:45 +0200

    send email to staff

--- src/app.js
+++ src/app.js
@@ -11,60 +11,139 @@ const { check, validationResult } = require('express-validator/check');
 
 const recaptcha = config.get('recaptcha');
 const redirect = config.get('redirect');
+const mailerConfig = config.get('nodemailer');
+
 const mailer = require('./mailer');
+const templates = require('./templates');
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
+debug(`list of available configs: [${config.get('sites')}]`);
 
-app.post('/', [
+app.get('/ecf', (req, res) => {
+  res.send('');
+});
+app.post('/ecf', [
   // username must be an email
-  check('email').isEmail(),
+  check('email').isEmail()
+    .normalizeEmail(),
   // password must be at least 5 chars long
   check('contents').isLength({
-    min: 5,
+    min: 1,
     max: 500,
-  })
-], (req, res) => {
+  }),
+
+  check('fullname').isLength({
+    min: 1,
+    max: 500,
+  }),
+
+  check('use').isIn(config.get('sites')),
+  check('lang').isIn(config.get('languages')),
+], (req, res, next) => {
+  debug('POST body:', req.body);
+  const site = req.body.use || 'default';
   // Finds the validation errors in this request and wraps them in an object with handy functions
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    if(redirect.BadRequest) {
-      return res.redirect(redirect.BadRequest);
-    } return res.status(404).json({
-      errors: errors.array()
+    const errorsArray = errors.array();
+    const errorsHash = errorsArray.map(d => `${d.param}=`).join('&');
+
+    debug('errors', errorsArray);
+    if (redirect[site].BadRequest) {
+      return res.redirect(`${redirect[site].BadRequest}?err=b&${errorsHash}`);
+    }
+    return res.status(404).json({
+      errors: errorsArray,
     });
   }
-  debug('POST remote:', req.connection.remoteAddress);
-  debug('POST body:', req.body);
+  return next();
+}, (req, res) => {
+  const site = req.body.use;
+  // RECAPTCHA request here
+  debug(`[${req.body.use}] POST remote: ${req.connection.remoteAddress}`);
+  debug(`[${req.body.use}] POST body:`, req.body);
 
   request({
     url: recaptcha.url,
     qs: {
-      secret: recaptcha.secret,
+      secret: recaptcha[req.body.use].secret,
       response: req.body['g-recaptcha-response'],
       remoteip: req.connection.remoteAddress,
     },
-  }, (err, recaptchaResponse, body) => {
+  }, async (err, recaptchaResponse, recaptchaBody) => {
     if (err) {
-      console.log(err);
-      return res.status(404).json({
-        message: 'generic error'
+      debug(err);
+      if (redirect[req.body.use].GenericError) {
+        return res.redirect(`${redirect[req.body.use].GenericError}?err=g`);
+      }
+      return res.status(500).json({
+        message: 'generic error',
       });
     }
-    const b = JSON.parse(body);
+    const b = JSON.parse(recaptchaBody);
 
     if (b.success !== undefined && !b.success) {
-      console.log(b)
+      console.log(b);
+      if (redirect[req.body.use].BadRequest) {
+        return res.redirect(`${redirect[req.body.use].BadRequest}?err=captcha`);
+      }
       return res.status(404).json({
-        message: 'Captcha validation failed'
+        message: 'Captcha validation failed',
       });
     }
-    if(redirect.Success) {
-      return res.redirect(redirect.Success);
-    }
-    res.header('Content-Type', 'application/json');
-    return res.send(b);
+
+    const tmp = templates({
+      name: 'notify',
+      language: 'en',
+    });
+
+    // mail!
+    const results = await Promise.all([
+      mailer({
+        from: req.body.email,
+        to: mailerConfig[site].to,
+        text: tmp.text({
+          content: req.body.content,
+        }),
+        html: tmp.html({
+          content: req.body.content,
+        }),
+        config: mailerConfig,
+      }),
+    ]).catch((mailerError) => {
+      debug('error', mailerError);
+      if (redirect[req.body.use].GenericError) {
+        return res.redirect(`${redirect[req.body.use].GenericError}?err=m`);
+      }
+      return res.status(500).json({
+        message: 'mail error',
+      });
+    }).then(promiseResponses =>{
+      console.log(promiseResponses);
+      if (redirect[req.body.use].Success) {
+        return res.redirect(redirect[req.body.use].Success);
+      }
+      return res.status(200).json({
+        ... b,
+        message: 'ok',
+      });
+    });
+
+
+    //
+    //
+    // .then(() => {
+    //   if (redirect[req.body.use].Success) {
+    //     res.redirect(redirect[req.body.use].Success);
+    //   } else {
+    //     res.header('Content-Type', 'application/json');
+    //     return res.send(b);
+    //   }
+    // })
+
+    return res.end();
   });
 });
 
